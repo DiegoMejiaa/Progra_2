6297c0a68e8675ea2a19fd3ddf8d90194cabeceb
DiegoMejiaa <145735814+DiegoMejiaa@users.noreply.github.com>
2023-09-28 09:20:19 -0600

    Update PruebaEstudiante.java

--- Cap3/PruebaEstudiante.java
+++ Cap3/PruebaEstudiante.java
@@ -1,29 +1,57 @@
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
 public class PruebaEstudiante {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedir información del estudiante
+        System.out.println("Ingrese el número de cuenta del estudiante:");
+        String numeroCuenta = scanner.nextLine();
+        System.out.println("Ingrese el nombre del estudiante:");
+        String nombre = scanner.nextLine();
+
+        // Ingresar la fecha de ingreso
+        Date fechaIngreso = ingresarFecha(scanner);
+        scanner.nextLine();
+
         // Crear un estudiante
-        Estudiante estudiante1 = new Estudiante(2022190021, "Edy Martinez");
+        Estudiante estudiante = new Estudiante(numeroCuenta, nombre, fechaIngreso);
 
-        // Matricular al estudiante en una materia
-        estudiante1.matricular("Ingles 3");
+        // Matricular al estudiante
+        System.out.println("Ingrese la materia a matricular:");
+        String materia = scanner.nextLine();
+        estudiante.matricular(materia);
 
-        // Establecer la calificación del estudiante
-        estudiante1.setCalificacion(80.7);
-
-        // Mostrar la información del estudiante
-        System.out.println("Información del Estudiante:");
-        estudiante1.mostrarInformacion();
-
-        // Crear otro estudiante
-        Estudiante estudiante2 = new Estudiante(2020170011, "Maria Rodriguez");
-
-        // Matricular al estudiante en una materia
-        estudiante2.matricular("Historia de Honduras");
-
-        // Establecer la calificación del estudiante
-        estudiante2.setCalificacion(40.0);
+        // Ingresar la calificación
+        System.out.println("Ingrese la calificación del estudiante:");
+        double calificacion = scanner.nextDouble();
+        estudiante.setCalificacion(calificacion);
 
         // Mostrar la información del estudiante
-        System.out.println("\nInformación del Estudiante 2:");
-        estudiante2.mostrarInformacion();
+        System.out.println("\nInformación del Estudiante:");
+        estudiante.mostrarInformacion();
+
+        scanner.close();
+    }
+
+    private static Date ingresarFecha(Scanner scanner) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        dateFormat.setLenient(false);
+        Date fecha = null;
+
+        while (fecha == null) {
+            try {
+                System.out.println("Ingrese la fecha de ingreso del estudiante (dd-MM-yyyy):");
+                String fechaStr = scanner.next();
+                fecha = dateFormat.parse(fechaStr);
+            } catch (ParseException e) {
+                System.out.println("Formato de fecha incorrecto. Por favor, use dd-MM-yyyy.");
+            }
+        }
+
+        return fecha;
     }
 }
