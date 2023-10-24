473f5706a49965b0ccd279911d8257abef868d01
DiegoMejiaa <145735814+DiegoMejiaa@users.noreply.github.com>
2023-10-24 10:31:38 -0600

    Update BoletoAereopuerto

--- Ejercicio2_15.java
+++ Ejercicio2_15.java
@@ -1,39 +1,37 @@
-// programa que recibe dos numeros, calcula e imprime su suma, diferencia,
- // producto y cociene
+class Aeropuerto {
+    private String nombrePasajero;
+    private String numeroVuelo;
+    private String destino;
+    private String fechaVuelo;
+    private String asiento;
 
- import java.util.Scanner;
+    public Aeropuerto(String nombrePasajero, String numeroVuelo, String destino, String fechaVuelo, String asiento) {
+        this.nombrePasajero = nombrePasajero;
+        this.numeroVuelo = numeroVuelo;
+        this.destino = destino;
+        this.fechaVuelo = fechaVuelo;
+        this.asiento = asiento;
+    }
 
- public class Ejercicio2_15
+    public void imprimirBoleto() {
+        System.out.println("****************************************");
+        System.out.println("         Boleto de Aeropuerto");
+        System.out.println("****************************************");
+        System.out.println("Nombre del Pasajero: " + nombrePasajero);
+        System.out.println("Número de Vuelo: " + numeroVuelo);
+        System.out.println("Destino: " + destino);
+        System.out.println("Fecha del Vuelo: " + fechaVuelo);
+        System.out.println("Asiento: " + asiento);
+        System.out.println("****************************************");
+    }
+}
 
- {        // Abre clase Ejercicio2.15
-  public static void main( String args[] )
-  {       // Abre metodo main
-  Scanner entrada = new Scanner(System.in);
+public class PruebaAeropuerto {
+    public static void main(String[] args) {
+        // Crear un boleto de aeropuerto con datos predefinidos
+        Aeropuerto boleto = new Aeropuerto("John Doe", "AA123", "Los Angeles", "2023-10-30", "12A");
 
-  int numero1;
-  int numero2;
-  int suma;
-  int producto;
-  int diferencia;
-  int cociente;
-
-  System.out.print("\nEste programa recibe dos numeros y da su producto, cociente, suma y diferencia.\n");
-
-  System.out.println("Introduzca el primer entero: ");
-  numero1 = entrada.nextInt();
-
-  System.out.println("Introduzca el segundo entero: ");
-  numero2 = entrada.nextInt();
-
-  suma = numero1 + numero2;
-  diferencia = numero1 - numero2;
-  producto = numero1*numero2;
-  cociente = numero1/numero2;
-
-  System.out.printf("\nLa suma de los numeros es: %d\n", suma);
-  System.out.printf("\nLa diferencia de los numeros es: %d\n", diferencia);
-  System.out.printf("\nEl producto de los numeros es: %d\n", producto);
-  System.out.printf("\nEl cociente de los numeros es: %d\n", cociente);
-
-   }       // Cierra metodo main
- }        // Cierra clase Ejercicio2.15
+        // Imprimir el boleto
+        boleto.imprimirBoleto();
+    }
+}
